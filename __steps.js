dd2edbad9fd34cbfdafeccef159bb54241e0efb1
subas-roy <subasroy46@gmail.com>
2024-11-26 00:18:47 +0600

    Basic project setup

---
+++ __steps.js
@@ -0,0 +1,30 @@
+/**
+ * 1. initialize a "package.json" file
+ *    --> npm init -y
+ * 2. install express, mongoose, cors, typescript, and cors
+ *    --> npm install express
+ *    --> npm install mongoose --save
+ *    --> npm install typescript --save-dev
+ *    --> npm install cors
+ * 3. for environment variables
+ *    --> npm i dotenv
+ * 4. initialize a typescript json file (tsconfig.json)
+ *    --> tsc -init
+ *    ------changes-----
+ *    "rootDir": "./src",
+ *    "outdir": "./dist"
+ *    create folder "src" in root
+ *    src/app.ts
+ * 5. adding eslint
+ *    --> npm i -D eslint@9.14.0 @eslint/js @types/eslint__js typescript typescript-eslint
+ *    --> npx eslint --init
+ *  6. adding prettier
+ *    --> npm i -D --exact prettier
+ *    Now create .prettierrc and .prettierignore file in the root of your project.
+ *    Include basic configurations for prettier in the .prettierrc file.
+ *    {
+        "semi": true,
+        "singleQuote": true
+      }
+ * 
+ */
