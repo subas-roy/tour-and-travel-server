703194148dffe2dcbe47591716cd7ada7da627a8
subas-roy <subasroy46@gmail.com>
2024-11-26 21:52:50 +0600

    Connect database and run on server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
+import mongoose from 'mongoose';
 import app from './app';
+import config from './config';
 
-function server() {}
-app.listen(5000, () => {
-  console.log('Server is running');
-});
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log(config.database_url);
 
 server();
