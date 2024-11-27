e1b745e5bb710a4d63a6581494ae2754c06774c1
subas-roy <subasroy46@gmail.com>
2024-11-27 16:26:21 +0600

    Implement schema validation

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -4,16 +4,32 @@ import { model, Schema } from 'mongoose';
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Please provide your name'],
+    minlength: 3,
+    maxlength: 50,
   },
-  age: { type: Number, required: true },
+  age: { type: Number, required: [true, 'Please enter your age'] },
   email: {
     type: String,
     required: true,
+    validate: {
+      validator: function (value: string) {
+        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2-6,}$/.test(value);
+      },
+      message: '{VALUE} is not a valid email',
+    },
+    unique: true,
   },
   photo: String,
-  role: { type: String, required: true },
-  userStatus: { type: String, required: true },
+  role: {
+    type: String,
+    enum: {
+      values: ['user', 'admin'],
+      message: '{VALUE} is not valid, please provide a valid role',
+    },
+    required: true,
+  },
+  userStatus: { type: String, enum: ['active', 'inactive'], required: true },
 });
 
 // Create Model
